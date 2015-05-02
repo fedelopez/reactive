25b0896ca9d0e2740b108e9e59b72782fab6aa07
fede <fede.lopez@gmail.com>
2015-05-02 21:58:54 +1000

    Week 3 - implemented "start"

--- src/main/scala/cat/pseudocodi/week3/nodescala.scala
+++ src/main/scala/cat/pseudocodi/week3/nodescala.scala
@@ -8,6 +8,7 @@ import nodescala._
 
 import scala.collection.JavaConversions._
 import scala.collection._
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
 
 /** Contains utilities common to the NodeScala© framework.
@@ -29,7 +30,12 @@ trait NodeScala {
     * @param token        the cancellation token
     * @param response     the response to write back
     */
-  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = ???
+  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = {
+    while (token.nonCancelled && response.hasNext) {
+      exchange.write(response.next())
+    }
+    exchange.close()
+  }
 
   /** A server:
     * 1) creates and starts an http listener
@@ -41,7 +47,29 @@ trait NodeScala {
     * @param handler        a function mapping a request to a response
     * @return               a subscription that can stop the server and all its asynchronous operations *entirely*
     */
-  def start(relativePath: String)(handler: Request => Response): Subscription = ???
+  def start(relativePath: String)(handler: Request => Response): Subscription = {
+    val listener = createListener(relativePath)
+    val listenerSubscription = listener.start()
+
+    val subscription = Future.run() { ct =>
+      Future {
+        while (ct.nonCancelled) {
+          val p = Promise[String]()
+          val request: Future[(Request, Exchange)] = listener.nextRequest()
+          request onComplete {
+            case tuple => Future {
+              respond(tuple.get._2, ct, handler(tuple.get._1))
+              p.success("Done request: " + tuple.get._1)
+            }
+          }
+          p.future.onComplete {
+            case res => println(res)
+          }
+        }
+      }
+    }
+    Subscription(subscription, listenerSubscription)
+  }
 
 }
 
